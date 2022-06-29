5789d4ed3e6a386cfec83be349a3943087db3c24
engelandr <andrea.engel@ict-campus.net>
2022-06-29 10:24:23 +0200

    added Layout.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
+import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
     return (
-        <Component {...pageProps} />
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
     )
 }
