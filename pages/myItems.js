50d037df18c11f1dbcd626e0bd5d5fa4eb581849
engelandr <andrea.engel@ict-campus.net>
2022-07-05 16:09:38 +0200

    make full view for admin

--- pages/myItems.js
+++ pages/myItems.js
@@ -46,6 +46,15 @@ export default function myItemsPage({items}){
                     })
                 }
             </div>
+            {session.username == "admin" && <h2>alle Angebote auf dieser Seite</h2> }    
+            {session.username == "admin" && 
+                <div className={styles.boxen}> 
+                    {items.map(item => {  
+                        return <BoxHome title = {item.title} price = {item.price} id = {item.id} key = {item.id} user = {item.user} image = {item.images[0].path}/> 
+                    })}
+                </div>
+            }
+            
         </div> : <p>Für diesen Bereich müssen Sie eingeloggt sein. <a href="/login">Hier</a> geht es zum Login.</p>}
         </>
         
