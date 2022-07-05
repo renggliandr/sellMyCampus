5918c56298154b72356201d5afc322f4d347e1f4
Andrin Renggli <andrin.renggli@ict-campus.net>
2022-07-05 10:08:32 +0200

    footer works

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,20 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Styles from "./Layout.module.css"
 
 
 export default function Layout({children}) {
 
     return (
         <>
+        <div className={Styles.allButFooter}>
             <Header></Header>
                 <main>     
                         <div>
                             {children}
                         </div>
                 </main>
+                </div>
             <Footer></Footer>
         </>
     )
